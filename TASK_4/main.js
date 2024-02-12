5c671f76d2dc8be34372c91eda50730db2717b48
shu <darja0604@gmail.com>
2024-02-12 10:38:18 +0300

    изучили циклы и вложенные циклы

---
+++ TASK_4/main.js
@@ -0,0 +1,33 @@
+// const students = ["Tom", "Bob", "Harry", "Ron"];
+
+// for (let i = 0; i < students.length; i++) {
+//     let result = students[i];
+//     console.log(result);
+// }
+
+// let people = [ ["Tom", 18], ["Bob", 17], ["Stan", 36] ];
+
+// for (let i = 0; i < people.length; i++) {
+//     for (let j = 0; j < people[i].length; j++) {
+//         let result = people[i][j];
+//         console.log(result);
+//     }    
+//     console.log("-----------");
+// }
+
+
+// const students = ["Tom", "Bob", "Harry", "Ron"];
+// //in - index
+// //of - element
+
+// for (let item in students) {
+//     console.log(students);
+// }
+
+
+const person = { name: "Tom", age: 23};
+console.log(person)
+
+for (let item in students) {
+    console.log(item, students[item]);
+}
